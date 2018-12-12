4edd168fd7cadfa52c788eec91888420c297ed1c
R็istino <ristinocoffee@gmail.com>
2018-12-11 21:27:37 -0600

    Fish Update
    Added fish AI and other misc stuff

---
+++ Project Anatinus/Assets/Anatinus/My Scripts/RISTINO SCIENCE.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class RISTINOSCIENCE : NetworkBehaviour
+{
+    public float speed = 10.0f;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        float horz = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed;
+        float vert = Input.GetAxisRaw("Vertical") * Time.deltaTime * speed;
+        transform.Translate(horz, vert, 0);
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
