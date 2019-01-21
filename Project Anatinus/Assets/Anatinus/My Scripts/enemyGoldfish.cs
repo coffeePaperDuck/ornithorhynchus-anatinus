fffaa71a5bd5f56e3f8bbf11522dd75a2894426d
_sirJames <bittestStrangest@gmail.com>
2019-01-21 14:20:04 -0800

    Goldfish and New Star

---
+++ Project Anatinus/Assets/Anatinus/My Scripts/enemyGoldfish.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyGoldfish : MonoBehaviour
+{
+    public float timer = 0.0f;
+    public Mesh goldfish1prefab;
+    public Mesh goldfish2prefab;
+    public Mesh goldfish3prefab;
+
+    // Use this for initialization
+    void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        timer += 1.0f * Time.deltaTime;
+        if (timer > 1.0f)
+        {
+            GetComponent<MeshFilter>().mesh = goldfish2prefab;
+        }
+        if (timer > 2.0f)
+        {
+            GetComponent<MeshFilter>().mesh = goldfish1prefab;
+        }
+    }
+}
