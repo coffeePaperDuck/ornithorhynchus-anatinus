39794c4a7f14d8ba7616b1533f6e5f8a288fd7f6
R็istino <ristinocoffee@gmail.com>
2019-03-10 06:57:51 -0500

    vdc

--- Project Anatinus/Assets/Anatinus/My Scripts/shootAtPosition.cs
+++ Project Anatinus/Assets/Anatinus/My Scripts/shootAtPosition.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class shootAtPosition : MonoBehaviour
 {
-    // Start is called before the first frame update
+  
+
+    float speed = 2f;
+
+    GameObject target;
+
+    Rigidbody rb;
+
+    Vector3 moveDirection;
+
+
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody> ();
+        target = GameObject.Find ("Player(Clone)");
+        moveDirection = (target.transform.position - transform.position).normalized * speed;
+        rb.velocity = new Vector3(moveDirection.x, moveDirection.y, moveDirection.z);
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
+
+
+
+
     }
+
+
+
+
+
 }
+    
