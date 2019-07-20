b2edd4b93fb7ac6848b1c87def234ce6003a788c
_sirJames <bittestStrangest@gmail.com>
2019-07-19 20:30:52 -0700

    small debris update

--- Project Anatinus/Assets/Anatinus/My Scripts/debris.cs
+++ Project Anatinus/Assets/Anatinus/My Scripts/debris.cs
@@ -9,7 +9,10 @@ public class debris : MonoBehaviour
     public float animationTimer = 0;
     public float animationTimerSpeed = 10;
     public float spin = 0;
+    public float spinAmount = 22.5f;
+    public float forwardSpeed = 10;
     public float ySpeed = 10;
+    public float yForce = 50;
 
     // Start is called before the first frame update
     void Start()
@@ -22,12 +25,14 @@ public class debris : MonoBehaviour
     {
         animationTimer += animationTimerSpeed * Time.deltaTime;
 
-        transform.Translate(0, ySpeed * Time.deltaTime, 0);
-        ySpeed -= 10 * Time.deltaTime;
+        transform.Translate(0, 0, forwardSpeed * Time.deltaTime);
+
+        transform.Translate(0, ySpeed * Time.deltaTime, 0, 0);
+        ySpeed -= yForce * Time.deltaTime;
 
         if (animationTimer > 1)
         {
-            spin += 22.5f;
+            spin += spinAmount;
             animationTimer = 0;
         }
 
